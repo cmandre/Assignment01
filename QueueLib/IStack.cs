11f5aea68176704621009f8afef1ccace9ce750b
cmandre <christop.andrews@mymhc.ca>
2021-01-20 16:17:30 -0700

    Checkpoint commit

---
+++ QueueLib/IStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueLib
+{
+    interface IStack <T>
+    {
+        T Push();
+
+        T Pop();
+
+        T Peek();
+
+        T Count();
+
+        T Empty();
+
+    }
+}
